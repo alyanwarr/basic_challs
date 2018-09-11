1ede188e6ff4d45a055a57e6ca933b5fedd07d28
Aly Anwar <alyanwarr@gmail.com>
2018-09-11 13:52:44 +0200

    Update lfi.php

--- lfi.php
+++ lfi.php
@@ -1,5 +1,5 @@
 <!-- http://localhost:8000/lfi.php?input=/etc/passwd 
-http://localhost:8000/lfi.php?input=php://filter/convert.base64-encode/resource=index.php -->
+Introudce the idea of wrappers: http://localhost:8000/lfi.php?input=php://filter/convert.base64-encode/resource=index.php -->
 
 <p> Usage example: http://localhost:8000/lfi.php?input=index </p>
 
